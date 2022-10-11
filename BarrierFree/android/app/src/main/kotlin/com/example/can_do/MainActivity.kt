753f3186aebffd524a4ca96ac928070fc357dfc3
jicongjy <82192951+jicongjy@users.noreply.github.com>
2022-10-11 20:14:10 +0900

    jiyoung

--- BarrierFree/android/app/src/main/kotlin/com/example/can_do/MainActivity.kt
+++ BarrierFree/android/app/src/main/kotlin/com/example/can_do/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.untitled1
+package com.example.can_do
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ BarrierFree/lib/can_do.dart
@@ -0,0 +1,31 @@
+import 'package:can_do/main.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vocabulary list',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('내 단어장'),
+        backgroundColor: Color(0xff323232),
+      ),
+    );
+  }
+}
