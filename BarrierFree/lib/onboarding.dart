8c72f859b8a6c157ec4c10d18f12978e2d0a699c
jicongjy <82192951+jicongjy@users.noreply.github.com>
2023-01-04 15:18:31 +0900

    onboarding

--- BarrierFree/lib/onboarding.dart
+++ BarrierFree/lib/onboarding.dart
@@ -11,25 +11,19 @@ class OnBoardingPage extends StatelessWidget {
     return IntroductionScreen(
       pages: [
         PageViewModel(
-            title: '퀴즈 풀고 포인트 받아가세요!',
-            body: 'This is the first page'
-                'We are making on-boarding screens.'
-                'It is very interesting!',
-            image: Image.asset('image/page1.png'),
+            title: '로그인을 통해 진행상황을 저장하세요.',
+            body: '아이디, 비밀번호를 입력하고 회원가입, 로그인하세요.',
+            image: Image.asset('image/1.png'),
             decoration: getPageDecoration()),
         PageViewModel(
-            title: '매일매일 나아지는 억양!',
-            body: 'This is the second page'
-                'We are making on-boarding screens.'
-                'It is very interesting!',
-            image: Image.asset('image/page2.png'),
+            title: '매일 매일 재밌는 단어학습!',
+            body: '단어 학습을 통해 남한어에 익숙해지세요!',
+            image: Image.asset('image/2.png'),
             decoration: getPageDecoration()),
         PageViewModel(
-            title: '북한어를 남한어로 번역해보세요!',
-            body: 'This is the third page'
-                'We are making on-boarding screens.'
-                'It is very interesting!',
-            image: Image.asset('image/page3.png'),
+            title: '캔두를 통해 남한에 성공적으로 정착하세요.',
+            body: '캔두는 탈북민 여러분의 언어의 장벽을 낮춰줍니다.',
+            image: Image.asset('image/3.png'),
             decoration: getPageDecoration()),
       ],
       done: const Text('done'),
@@ -42,21 +36,28 @@ class OnBoardingPage extends StatelessWidget {
       showSkipButton: true,
       skip: const Text('skip'),
       dotsDecorator: DotsDecorator(
-          color: Colors.cyan,
+          color: Color(0xff6DB193),
           size: const Size(10, 10),
           activeSize: const Size(22, 10),
           activeShape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-          activeColor: Colors.red),
+          activeColor: Colors.blue),
       curve: Curves.bounceOut,
     );
   }
 
   PageDecoration getPageDecoration() {
     return const PageDecoration(
-        titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        bodyTextStyle: TextStyle(fontSize: 18, color: Colors.blue),
+        titleTextStyle: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Pretendard'),
+        bodyTextStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: Color(0xff6DB193),
+            fontFamily: 'Pretendard'),
         imagePadding: EdgeInsets.only(top: 40),
-        pageColor: Colors.orange);
+        pageColor: Colors.white);
   }
 }
